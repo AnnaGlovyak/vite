a78bcc8647570bc1264c512cd6a231485ac2d17c
Анна Гловяк <anna.kmeb@gmail.com>
2023-11-30 16:12:49 +0200

    svg as react component'

--- custom-react-app/src/App.tsx
+++ custom-react-app/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import ViteLogo from "./assets/vitelogo.png";
+import AppleLogo from "./assets/apple.svg?react";
 
 const App = () => {
     return (
-        <div> Hello World </div>
+        <div> Hello World 
+            <img src={ViteLogo} />
+            <AppleLogo></AppleLogo>
+        </div>
     )
 }
 

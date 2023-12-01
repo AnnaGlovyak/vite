c4a9ee948d4d6b37a7adfbe0e28e543257eac609
Анна Гловяк <anna.kmeb@gmail.com>
2023-12-01 17:18:37 +0200

    ui lib, react simple app

--- custom-react-app/vite.config.js
+++ custom-react-app/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import reactPlugin from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import {resolve} from "path";
 
 export default defineConfig({
-    plugins: [reactPlugin(), svgr({exportAsDefault: true})]
+    plugins: [reactPlugin(), svgr({exportAsDefault: true})],
+    build: {
+        lib: {
+            entry: resolve("src", "components"),
+            formats: ["es"],
+            name: "uiLib",
+            fileName: "uiLib"
+        }
+    }
 })

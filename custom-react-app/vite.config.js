0d8bac676da46c5352b3d41c176ef4af6b1e40dd
Анна Гловяк <anna.kmeb@gmail.com>
2023-12-03 17:46:58 +0200

    css into js, plugin for my ui lib

--- custom-react-app/vite.config.js
+++ custom-react-app/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import reactPlugin from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import svgr from "vite-plugin-svgr";
 import {resolve} from "path";
 
 export default defineConfig({
-    plugins: [reactPlugin(), svgr({exportAsDefault: true})],
+    plugins: [reactPlugin(), svgr({exportAsDefault: true}), cssInjectedByJsPlugin()],
     build: {
         lib: {
             entry: resolve("src", "components"),
